ce295c2e1788fefff16e03874f0de36ed08c384a
trang.nguyen <trang.nguyen@LGVN14343>
2018-04-27 15:24:06 +0700

    new code

---
+++ TA_Dashboard/Actions/LoginPage/LoginPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TA_Dashboard.Actions.LoginPage
+{
+    class LoginPage
+    {
+    }
+}
